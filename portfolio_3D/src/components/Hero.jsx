2a80e5ba992c43ec7637b58194b4a1a7517d321a
Ravi K. Kispotta <ravikantkispotta2@gmail.com>
2024-10-21 15:43:13 +0530

    Added a 3D model to the hero Section

--- portfolio_3D/src/components/Hero.jsx
+++ portfolio_3D/src/components/Hero.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { Suspense, useEffect } from 'react'
 import { styles } from '../styles'
 import DynamicText from './DynamicText'
+import { motion } from 'framer-motion'
+import { Canvas } from '@react-three/fiber'
+import Laptop from '../models/Laptop'
+import Loader from '../utils/Loader'
+
 
 const Hero = () => {
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const myElement = document.getElementById('heroModel');
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      if (scrollY > 400) { // Adjust the scroll value as needed
+        myElement.style.opacity = '0';
+      } else {
+        myElement.style.opacity = '1';
+      }
+    };
+    
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup the event listener on component unmount
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <section className='relative w-full h-screen mx-auto'>
-
-      <div className={`${styles.paddingX} absolute top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+    <div className='max-w-7xl mx-auto flex flex-row items-start gap-5'>
+        <div className={`${styles.paddingX} absolute top-[120px]`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-prime' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
@@ -27,8 +50,57 @@ const Hero = () => {
           to improve my skills further and this is just the
           starting.</p>
       </div>
+    </div>
       
+    <span className='lg:w-1/3 w-full lg:h-full h-[500px] fixed z-1 lg:mr-20 right-0 lg:top-0 top-2/4 transition-opacity duration-700' id='heroModel'>
+        {/* Canvas */}
+      <Canvas
+          camera={{
+            position: [0, 0, 5],
+            fov: 75,
+            near: 0.1,
+            far: 1000,
+          }}        
+        >
+          <directionalLight position={[0, 0, 1]} intensity={2.5} />
+          <ambientLight intensity={1} />
+          <pointLight position={[5, 10, 0]} intensity={2} />
+          <spotLight
+            position={[10, 10, 10]}
+            angle={0.15}
+            penumbra={1}
+            intensity={2}
+          />
 
+          <Suspense fallback={<Loader />}>
+            <Laptop 
+              position={[-0.1, -1.8, -0.35]}
+              rotation={[12.629, -0.5, 0]}
+              scale={[1.4, 1.4, 1.4]}
+              fov={[24]}
+            />
+          </Suspense>
+        </Canvas>
+    </span>
+
+      <div className='absolute xs:bottom-10 bottom-20 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
+    
     </section>
   )
 }
--- portfolio_3D/src/utils/Loader.jsx
+++ portfolio_3D/src/utils/Loader.jsx
@@ -1,12 +1,32 @@
-import { Html } from "@react-three/drei";
+import { Html, useProgress } from "@react-three/drei";
 
 const Loader = () => {
+  const {progress} = useProgress();
   return (
-    <Html>
-      <div className='flex justify-center items-center'>
-        <div className='w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin'></div>
-      </div>
+
+    <Html
+      as='div'
+      center
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <span className='w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 animate-spin'></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: "#F1F1F1",
+          fontWeight: 800,
+          marginTop: 40,
+        }}
+      >
+        {progress.toFixed(2)}%
+      </p>
     </Html>
+
   );
 };
 
